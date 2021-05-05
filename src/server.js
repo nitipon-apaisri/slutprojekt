6c1bc1e3134abbed48064a70b3eddbe82039e300
richard-iths <richard.forsberg@iths.se>
2021-05-05 20:22:54 +0200

    added logger middleware and connected it to server only if node_env is dev

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
+// custom middlewares
+const loggerMiddleware = require("./middlewares/logger");
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "dev") {
+  app.use(loggerMiddleware);
+}
 
 app.get("/", (req, res, next) => {
   res.json({ message: "welcome" });
