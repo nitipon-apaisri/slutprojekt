ccb940a31b047302db3f3edc375cd4b49ffcac9d
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-05-06 11:32:19 +0200

    Added bcrypt for hasing password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,12 @@
+const bcrypt = require('bcryptjs')
 const userModel = require('../models/userModel')
 
 const createUser = async (req, res) => {
   const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 10)
   const newUser = await userModel({
     username: username,
-    password: password
+    password: hash
   })
   await newUser.save()
   res.json({ message: 'Success', data: newUser })
