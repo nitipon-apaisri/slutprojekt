c4a4f1faef1c492775a04f3c04f8a28b20ae2ef5
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-05-06 09:47:57 +0200

    Added database connection

---
+++ src/database/connection.js
@@ -0,0 +1,10 @@
+const { MongoMemoryServer } = require('mongodb-memory-server')
+const mongoose = require('mongoose')
+const mongod = new MongoMemoryServer()
+
+async function connect() {
+  const uri = await mongod.getUri()
+  const options = { useNewUrlParser: true, useUnifiedTopology: true }
+  return mongoose.connect(uri, options)
+}
+module.exports = connect
