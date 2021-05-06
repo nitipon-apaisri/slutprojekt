1a9c2546a6e532103b3db3c04047ceb299211698
richard-iths <richard.forsberg@iths.se>
2021-05-06 14:08:19 +0200

    added error models for invalid body and unauthorized,  added funtionality in auth middleware for rbac and connected custom error messages

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,15 +1,14 @@
-const BaseError = require("../models/errors/BaseError");
+const BaseError = require('../models/errors/base')
 module.exports = (error, req, res, next) => {
   if (error instanceof BaseError) {
-    return res.status(error.errorCode).json({ error: error.message });
+    return res.status(error.errorCode).json({ error: error.message })
   }
 
-  if (process.env.NODE_ENV === "dev") {
-    return res.status(500).json({ error: error.message });
+  if (process.env.NODE_ENV === 'dev') {
+    return res.status(500).json({ error: error.message })
   }
 
   return res.status(500).json({
-    error:
-      "Something went wrong, please contact an admin if the error persists",
-  });
-};
+    error: 'Something went wrong, please contact an admin if the error persists'
+  })
+}
