06085776936691bf35204d995ecb25f1a898ef9c
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-05-10 09:52:10 +0200

    Forgot to save a file

--- src/routes/genericRoutes.js
+++ src/routes/genericRoutes.js
@@ -4,7 +4,6 @@ const userController = require('../controllers/userController')
 const router = Router()
 
 router.post('/auth', userController.signIn)
-router.get('/users/:id', userController.getUserById)
 router.get('/me', authMiddleware.authorization)
 
 module.exports = router
