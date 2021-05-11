c340392f43d72b79d4bbb367f4201bab5b016a79
richard-iths <richard.forsberg@iths.se>
2021-05-11 14:15:01 +0200

    extracted role access to its own file, cstructured up the fileupload config, added the new access middleware to routes and implemented custom errors in file upload,auth middleware and user access middleware

---
+++ src/middlewares/userAccess.js
@@ -0,0 +1,38 @@
+const authError = require('../models/errors/unauthorized')
+
+const Role = {
+  ADMIN: 'admin',
+  CLIENT: 'client',
+  WORKER: 'worker'
+}
+
+const authInvalidAccessError = () => {
+  throw new authError.Unauthorized(
+    authError.ErrorMessage.FORBIDDEN_INVALID_ACCESS
+  )
+}
+
+const hasRole = (role, ...roles) => roles.includes(role)
+
+const workerAccess = (req, res, next) =>
+  hasRole(req.user.role, Role.WORKER) ? next() : authInvalidAccessError()
+
+const workerAndClientAccess = (req, res, next) =>
+  hasRole(req.user.role, Role.WORKER, Role.CLIENT)
+    ? next()
+    : authInvalidAccessError()
+
+const workerAndAdminAccess = (req, res, next) =>
+  hasRole(req.user.role, Role.WORKER, Role.ADMIN)
+    ? next()
+    : authInvalidAccessError()
+
+const adminAccess = (req, res, next) =>
+  hasRole(req.user.role, Role.ADMIN) ? next() : authInvalidAccessError()
+
+module.exports = {
+  workerAccess,
+  workerAndClientAccess,
+  workerAndAdminAccess,
+  adminAccess
+}
