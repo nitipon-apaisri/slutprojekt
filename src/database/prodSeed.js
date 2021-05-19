36f4a6ee4eb7486cf93fae14c462e18179a8771b
richard-iths <richard.forsberg@iths.se>
2021-05-19 15:12:22 +0200

    Revert "fixed prod seed script again"
    This reverts commit 701f64d96d29c9659fb64a73e3a752adb38e795d.

--- src/database/prodSeed.js
+++ src/database/prodSeed.js
@@ -1,17 +1,12 @@
 const { prodConnect } = require('./connection')
 const userModel = require('../models/userModel')
 
-;(async () => {
+productionSeed = async () => {
   await prodConnect()
-  try {
-    const user = await userModel.create({
-      username: 'admin',
-      password: process.env.SEEDED_ADMIN_PASSWORD,
-      role: 'admin'
-    })
-    await user.save()
-    process.exit(0)
-  } catch (error) {
-    console.log(error)
-  }
-})()
+  const user = await userModel.create({
+    username: 'admin',
+    password: process.env.SEEDED_ADMIN_PASSWORD
+  })
+  await user.save()
+  process.exit(0)
+}
