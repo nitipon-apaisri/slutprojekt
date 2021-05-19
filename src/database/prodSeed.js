db869aee1b88a6954ea2a29b5002ea34bd93e734
richard-iths <richard.forsberg@iths.se>
2021-05-19 15:17:05 +0200

    reverted to old commit

--- src/database/prodSeed.js
+++ src/database/prodSeed.js
@@ -1,12 +1,11 @@
 const { prodConnect } = require('./connection')
-const userModel = require('../models/userModel')
-
-productionSeed = async () => {
+const userModel = require('../models/userModel')(() => {
   await prodConnect()
   const user = await userModel.create({
     username: 'admin',
-    password: process.env.SEEDED_ADMIN_PASSWORD
+    password: process.env.SEEDED_ADMIN_PASSWORD,
+    role: 'admin'
   })
   await user.save()
   process.exit(0)
-}
+})()
